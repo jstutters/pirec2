f9f31fac36b4fd895de29ed3e014d7384c7e3a52
Jon Stutters <j.stutters@ucl.ac.uk>
2018-01-04 15:29:17 +0000

    Delete test files

--- testload.py
+++
@@ -1,8 +0,0 @@
-import pirec2.task
-
-with open('pipeline.json', 'r') as f:
-    pipeline = pirec2.task.Pipeline.load(f)
-pipeline.run()
-print(pipeline.root_node.result.value)
-# with open('pipeline.json', 'w') as f:
-    # pipeline.save(f)
